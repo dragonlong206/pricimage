31403e5e727ecdeff2e6ff661508e8cca12d09cc
Long <lv.long.tn@gmail.com>
2020-05-22 11:32:08 +0700

    Prototyping UI

---
+++ Pricimage/HistoryItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pricimage
+{
+    public class HistoryItem
+    {
+        public string Code { get; set; }
+
+        public double? OldPrice { get; set; }
+
+        public double NewPrice { get; set; }
+
+        public string ImageSource
+        { 
+            get
+            {
+                return "/Images/" + Code + ".jpg";
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                string result = Code + ": ";
+                if (OldPrice.HasValue)
+                {
+                    result += OldPrice.Value.ToString("n2") + " ~> ";
+                }
+                result += NewPrice.ToString("n2");
+
+                return result;
+            }
+        }
+    }
+}
--- Pricimage/MainWindow.xaml.cs
+++ Pricimage/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,6 +25,44 @@ namespace Pricimage
         public MainWindow()
         {
             InitializeComponent();
+
+            List<HistoryItem> historyItems = new List<HistoryItem>
+            {
+                new HistoryItem{ Code = "02324590", NewPrice = 68},
+                new HistoryItem{ Code = "05424654", OldPrice = 256.8, NewPrice = 245},
+                new HistoryItem{ Code = "02324590", NewPrice = 68},
+                new HistoryItem{ Code = "05424654", OldPrice = 256.8, NewPrice = 245},
+                new HistoryItem{ Code = "02324590", NewPrice = 68},
+                new HistoryItem{ Code = "05424654", OldPrice = 256.8, NewPrice = 245},
+                new HistoryItem{ Code = "02324590", NewPrice = 68},
+                new HistoryItem{ Code = "05424654", OldPrice = 256.8, NewPrice = 245},
+            };
+            lstHistory.ItemsSource = historyItems;
+
+            cboCode.ItemsSource = ReadFileList();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private List<string> ReadFileList()
+        {
+            List<string> result = new List<string>();
+
+            var imageFiles = Directory.GetFiles(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images"));
+            if (imageFiles != null && imageFiles.Any())
+            {
+                foreach (var item in imageFiles)
+                {
+                    var code = System.IO.Path.GetFileNameWithoutExtension(item);
+                    result.Add(code);
+                }
+            }
+
+            return result;
         }
     }
 }
